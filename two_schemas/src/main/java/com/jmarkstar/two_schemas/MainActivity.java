9fd8db85209cdb5aaecda094881882db88d6375e
jmarkstar <mestrella.1991@gmail.com>
2017-05-22 18:33:43 -0500

    adding two schemas module.

---
+++ two_schemas/src/main/java/com/jmarkstar/two_schemas/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jmarkstar.two_schemas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author jmarkstar
+ * */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
